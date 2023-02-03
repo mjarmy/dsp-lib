f19940e3bab4c3cc719a32636390e18cb38bd24f
Mike Jarmy <mjarmy@gmail.com>
2023-02-03 09:11:38 -0500

    create SvfInputMixing

---
+++ filters/SvfInputMixing.hpp
@@ -0,0 +1,138 @@
+#pragma once
+
+#include <cmath>
+
+//--------------------------------------------------------------
+//
+// SvfInputMixing is a State Variable Filter that applies a configureable mix of
+// lowpass, bandpass and highpass filtering.  It emulates analog electronic
+// circuits found in classic synthesizers.
+//
+// https://en.wikipedia.org/wiki/State_variable_filter
+//
+// SvfInputMixing is a C++ port of the algorithm described in the paper "Input
+// mixing linear trapezoidal State Variable Filter (SVF) in state increment
+// form", by Andy Simper of Cytomic.
+//
+// https://cytomic.com/files/dsp/SvfInputMixing.pdf
+//
+//--------------------------------------------------------------
+
+class SvfInputMixing {
+
+  public:
+
+    SvfInputMixing() {}
+    ~SvfInputMixing() {}
+
+    // Call init() any time you'd like to change the cutoff frequency,
+    // resonance, or sample rate.  If you are changing the sample rate, you
+    // should call clear() before calling init().
+    //
+    // cutoff: cutoff frequency -- In Hz, range is [16.0, sampleRate/2.0].
+    // res: resonance, aka Q -- Range is [0.0, 0.999].
+    // sampleRate -- samples per second, e.g. 44100.
+    //
+    void init(double cutoff, double res, double sampleRate) {
+
+        double k = 2 - 2 * res;
+        double w = M_PI * cutoff / sampleRate;
+        double s1 = std::sin(w);
+        double s2 = std::sin(2 * w);
+        double nrm = 1 / (2 + (2 - k) * s2);
+        double s1n = 2 * s1 * s1 * nrm;
+        double s2n = s2 * nrm;
+
+        g0m0 = k * s1n + s2n;
+        g0m1 = -s1n;
+        g0m2 = -s2n;
+        g1 = -s1n;
+        g2 = -s2n;
+        g3m0 = s1n;
+        g3m1 = s2n;
+        g3m2 = -s1n - k * s2n;
+        g4 = s2n;
+        g5 = -s1n - k * s2n;
+    }
+
+    // Call setMix() any time you'd like to change the output mixture of
+    // lowpass, bandpass and highpass filtering.
+    //
+    // In "normal" operation, the sum of the three parameters should
+    // be 1.0, e.g.:
+    //
+    //      lowpass only: [1, 0, 0]
+    //      bandpass only: [0, 1, 0]
+    //      highpass only: [0, 0, 1]
+    //      even mix of lowpass and highpass: [0.5, 0.0, 0.5]
+    //      mostly lowpass plus a little bandpass: [0.9, 0.1, 0]
+    //      etc, etc.
+    //
+    // Per Andy Simper's paper, you can also create a notch or peaking filter:
+    //
+    //      notch: [1, 0, 1]
+    //      peaking: [-1, 0, 1]
+    //
+    void setMix(double lowMix_, double bandMix_, double highMix_) {
+        lowMix = lowMix_;
+        bandMix = bandMix_;
+        highMix = highMix_;
+    }
+
+    // Call clear() before changing the sample rate, or any time you need to
+    // reset the filter.
+    void clear() {
+        ic1eq = 0.0;
+        ic2eq = 0.0;
+    }
+
+    // Call tick() per-sample to filter the input.
+    double tick(double input) {
+
+        double vlow = lowMix * input;
+        double vband = bandMix * input;
+        double vhigh = highMix * input;
+
+        double t1 =
+            vlow * g0m0 + vband * g0m1 + vhigh * g0m2 + g1 * ic1eq + g2 * ic2eq;
+        double t2 =
+            vlow * g3m0 + vband * g3m1 + vhigh * g3m2 + g4 * ic1eq + g5 * ic2eq;
+        double vc2 = t2 + ic2eq;
+        ic1eq = ic1eq + 2.0 * t1;
+        ic2eq = ic2eq + 2.0 * t2;
+
+        double output = vhigh + vc2;
+
+        // It's possible for the filter to start producing NaN output if you
+        // call init() with wildy varying extreme values for cutoff and
+        // resonance, in between successive calls to tick().  This check is a
+        // failsafe mechanism to reset the filter if that happens.  You can
+        // comment it out if you don't need it.
+        if (std::isnan(output)) {
+            clear();
+            return 0;
+        } else {
+            return output;
+        }
+    }
+
+  private:
+
+    double g0m0 = 0.0;
+    double g0m1 = 0.0;
+    double g0m2 = 0.0;
+    double g1 = 0.0;
+    double g2 = 0.0;
+    double g3m0 = 0.0;
+    double g3m1 = 0.0;
+    double g3m2 = 0.0;
+    double g4 = 0.0;
+    double g5 = 0.0;
+
+    double lowMix = 0.0;
+    double bandMix = 0.0;
+    double highMix = 0.0;
+
+    double ic1eq = 0.0;
+    double ic2eq = 0.0;
+};
